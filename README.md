f9629f4e0ba0708611f42aeb772635c5f8369446
Eduardo <egonzalez@paradigmadigital.com>
2017-11-28 21:22:12 +0100

    Readme and config
    Readme and config

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # consul-request-service
+
+Solicita información a consul-response-service preguntando a Consul su dirección por nombre de servicio. No se registra en Consul como miembro y recupera sus propiedades de él.
+
+## Arrancar el docker
+
+Desde la raiz del proyecto:
+
+./gradlew clean build
+
+docker build -f Dockerfile .
+
+docker run <ID_DOCKER>
--- src/main/java/es/paradigmadigital/consulrequest/controller/ConsulRequestController.java
+++ src/main/java/es/paradigmadigital/consulrequest/controller/ConsulRequestController.java
@@ -3,8 +3,10 @@ package es.paradigmadigital.consulrequest.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/consul-request-service")
+@RequestMapping("/consul-request")
 public class ConsulRequestController {
 
 	@Autowired
@@ -21,8 +23,14 @@ public class ConsulRequestController {
 	@Autowired
 	DiscoveryClient discoveryClient;
 
+	@Value("${key_3}")
+	private String key3;
+	
+	@Value("${key_4}")
+	private String key4;
+	
 	@GetMapping()
-	public ResponseEntity<ResponseDTO> getResponse(String name) {
+	public ResponseEntity<ResponseDTO> getResponse() {
 		
 		Optional<ServiceInstance> optionalServiceInstance = discoveryClient.getInstances("consul-response-service")
 				.stream().findFirst();
@@ -34,6 +42,12 @@ public class ConsulRequestController {
 		ResponseEntity<ResponseDTO> response = rest.getForEntity("http://consul-response-service/consul-response",
 				ResponseDTO.class);
 
+		return response;
+	}
+	
+	@GetMapping(path = "/config")
+	public ResponseEntity<String> getConfig(String name) {
+		ResponseEntity<String> response = new ResponseEntity<String>(key3 + " " + key4, HttpStatus.OK);
 		return response;
 	}
 }
